e4395d6e21e1a1e539940ffe32f853efd8754fc0
SoftechMansoor <softech1900@gmail.com>
2021-05-26 21:06:06 +0500

    Add files via upload
    Using-Include-Function-to-Display-Menu-in-PHP

---
+++ contact.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Include</title>
+</head>
+<body>
+<?php
+
+include 'menu.php';
+
+?>
+
+<div class="page">
+	<h1>Contact Us</h1>
+</div>
+
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Menu</title>
+<style>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #BA4A00;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover {
+  background-color: #111;
+}
+
+
+
+
+
+</style>
+
+</head>
+<body>
+<header>
+	<nav>
+		<ul>
+		<li><a href="home.php">Home</a></li>
+		<li><a href="about.php">About Us</a></li>
+		<li><a href="contact.php">Contact Us</a></li>
+        </ul>
+	</nav>
+</header>
+</body>
+</html>
+
+
+
